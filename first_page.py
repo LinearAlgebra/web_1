b88f9d6ac0c58bfbcf4aa8cd19bdf99fda280b8f
Taitong Xie <412313393@qq.com>
2016-09-11 12:24:23 +0800

    +1s

--- first_page.py
+++ first_page.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import abort
 from flask import redirect
-import os, sys
+import os, sys, urllib.request
 sys.path.append('./web_model')
 
 import train_monitor, emailsender
@@ -12,9 +12,14 @@ app = Flask(__name__)
 def index():
 	return '<h1> Hello World!</h1>'
 
-@app.route('/xtt/<order>')
+@app.route('/train_monitor/<order>')
 def run_order(order):
-	os.system(order)
+	order_str = urllib.request.unquote(order)
+	order_list = order_str.split(' ')
+	try:
+		train_monitor.train_monitor(order_list[0], order_list[1], order_list[2], order_list[3])
+	except BaseException as e:
+		return '<h1> 抓取错误 %s </h1>' % e
 
 if __name__=='__main__':
 	app.run(debug=True, host = '0.0.0.0', port = 8421)
