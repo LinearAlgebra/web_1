73abcdab3e15350f0884c322d6cff506464396dd
Taitong Xie <412313393@qq.com>
2016-09-12 15:26:07 +0800

    +1s

--- personal_information.py
+++ personal_information.py
@@ -58,4 +58,4 @@ class Detail_Info(db.Model):
 		return '<Phonenumber %r>' % self.phonenumber
 
 if __name__ == '__main__':
-	app.run(debug=True,host='0.0.0.0',port=8842)
+	app.run(host='0.0.0.0',port=8422)
