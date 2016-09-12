dee002000596a2a933a00528570726ec06faabb2
Taitong Xie <412313393@qq.com>
2016-09-12 14:31:57 +0800

    +1s

--- personal_information.py
+++ personal_information.py
@@ -1,13 +1,16 @@
+#-*-utf8-*-
+
 from flask import Flask, render_template
 from flask_script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField, IntegerField
-from wtforms.validators import Required, Email, NumberRange
+from wtforms.validators import Required, Length
 from flask_script import Manager
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 manager = Manager(app)
 db = SQLAlchemy(app)
 
@@ -19,28 +22,28 @@ app.config['SECRET_KEY'] = 'EASY TO GUESS'
 def index():
 	form = InformationForm()
 	if form.validate_on_submit():
-		student = Student.query(student_number=form.name.data).first()
+		student = Student.query.filter_by(student_number=form.student_number.data).first()
+		print(form.student_number.data)
 		if student is None:
 			student = Student(student_number=form.student_number.data,name=form.name.data)
 			detail = Detail_Info(phone_number=form.phone_number.data,identity=student)
-			db.session.add([student,detail])
+			db.session.add_all([student,detail])
 			db.session.commit()
 			return '<h1> 2333 </h1>'
 			session['known'] = False
-		if student:
+		else:
 			return '<h1> Yes </h1>'
 	return render_template('personal_information.html', form=form)
 
-
 class InformationForm(Form):
-	student_number = IntegerField("请输入学号:", validators=[Required()])
+	student_number = StringField("请输入学号:", validators=[Length(10)])
 	name = StringField("请输入姓名:", validators=[Required()])
-	phone_number = IntegerField("请输入电话号码:", validators=[Required()])
+	phone_number = StringField("请输入电话号码:", validators=[Length(11)])
 	submit = SubmitField("Submit")
 
 class Student(db.Model):
 	__tablename__ = 'students'
-	student_number = db.Column(db.String(64), primary_key=True,unique=True)
+	student_number = db.Column(db.String(10), primary_key=True,unique=True)
 	name = db.Column(db.String(64))
 	identity = db.relationship('Detail_Info', backref='identity')
 
@@ -49,8 +52,8 @@ class Student(db.Model):
 
 class Detail_Info(db.Model):
 	__tablename__ = 'detail_info'
-	student_number = db.Column(db.String(64), db.ForeignKey('students.student_number'),primary_key=True,unique=True)
-	phone_number = db.Column(db.String(64))
+	student_number = db.Column(db.String(10), db.ForeignKey('students.student_number'),primary_key=True,unique=True)
+	phone_number = db.Column(db.String(11))
 
 	def __repr__(self):
 		return '<Phonenumber %r>' % self.phonenumber
