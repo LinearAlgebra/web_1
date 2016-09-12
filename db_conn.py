7f538f4ac6a678b5e2147219a7e89b3186e858a2
Taitong Xie <412313393@qq.com>
2016-09-12 11:08:07 +0800

    +1s

--- db_conn.py
+++ db_conn.py
@@ -1,7 +1,32 @@
-import MySQLdb
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_script import Manager
 
+app = Flask(__name__)
+manager = Manager(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12518ll+.@52.23.150.84:3306/test1'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+# conn = MySQLdb.connect(host='52.23.150.84',port=3306,user='root',passwd='12518ll+.',db='mysql')
 
-a =  'mysql://root:12518ll+.@http://52.23.150.84:3306/mysql'
-conn = MySQLdb.connect(host='52.23.150.84',port=3306,user='root',passwd='12518ll+.',db='mysql')
+db = SQLAlchemy(app)
 
-cur = conn.cursor()
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	users = db.relationship('User', backref='role')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+if __name__ == '__main__':
+	manager.run()
