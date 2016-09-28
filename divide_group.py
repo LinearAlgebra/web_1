fe165f02f15ad5247b9b5ce6235ae27cab998a81
Taitong Xie <412313393@qq.com>
2016-09-28 14:13:23 +0800

    +1s

--- divide_group.py
+++ divide_group.py
@@ -22,6 +22,7 @@ app.config['SECRET_KEY'] = 'EASY TO GUESS'
 def index():
 	form = InformationForm()
 	form_1 = QueryForm()
+	db.connect()
 	student_data = db.session.query(Student).all()
 	if form.validate_on_submit():
 		student = Student.query.filter_by(student_number=form.student_number.data).first()
