9ab5ce2adc4dff900ea361e74cdad25c1494aed9
Taitong Xie <412313393@qq.com>
2016-09-28 17:30:01 +0800

    +1s

--- mail_receiver.py
+++ mail_receiver.py
@@ -3,7 +3,8 @@ from poplib import POP3_SSL
 import os, email, smtplib, pickle, time
 from sqlalchemy import Column, String, create_engine
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 # engine = create_engine('mysql+mysqlconnector://root:12518ll+.@localhost:3306/test1')
 # db = sessionmaker(bind=engine)
 # session = db()
@@ -15,8 +16,8 @@ revcSer = POP3_SSL(pop3server)
 # revcSer.starttls()
 revcSer.user('nkjz2016@126.com')  
 revcSer.pass_('nkjz2016')
-
 num,total_size = revcSer.stat()
+
 
 dir_list = os.listdir('./')
 if 'record.txt' not in dir_list:
@@ -30,39 +31,60 @@ if 'record.txt' not in dir_list:
 		f = open('record.txt', 'wb')
 		pickle.dump(record_list, f)
 		f.close()
-	time.sleep(300)
-while True:
-	f = open('record.txt', 'rb')
-	record_list = pickle.load(f)
-	f.close()
-	num,total_size = revcSer.stat()
-	while True:
-		hdr,text,octet=revcSer.retr(num)
-		full_mail = map(bytes.decode, text)
-		content = '\n'.join(full_mail)
-		msg = email.message_from_string(content)
-		smtpObj = smtplib.SMTP(smtpserver)
-		smtpObj.login('nkjz2016@126.com','nkjz2016')
-		if msg.get('Message-ID') in record_list:
-			break
-		else:
-			engine = create_engine('mysql+mysqlconnector://root:12518ll+.@localhost:3306/test1')
-			db = sessionmaker(bind=engine)
-			break
-			session = db()
-			student = session.query(Student).all()
-			print(student)
-			smtpObj.sendmail('nkjz2016@126.com',['412313393@qq.com','814453212@qq.com'],msg.as_string())
-			record_list.append(msg.get('Message-ID'))
-			num -= 1
-	break
-	smtpObj.quit()
-	f = open('record.txt', 'wb')
-	pickle.dump(record_list, f)
-	f.close()
+		revcSer.close()
 	time.sleep(300)
 
-class Student(db.Model):
+class Student(Base):
 	__tablename__ = 'student_email'
 	student_number = Column(String(10), primary_key=True,unique=True)
 	name = Column(String(64))
+
+def main():
+	while True:
+		f = open('record.txt', 'rb')
+		record_list = pickle.load(f)
+		f.close()
+		revcSer = POP3_SSL(pop3server)
+		revcSer.user('nkjz2016@126.com')  
+		revcSer.pass_('nkjz2016')
+		num,total_size = revcSer.stat()
+		while True:
+			hdr,text,octet=revcSer.retr(num)
+			full_mail = map(bytes.decode, text)
+			content = '\n'.join(full_mail)
+			msg = email.message_from_string(content)
+			smtpObj = smtplib.SMTP('smtp.qq.com',587)
+			smtpObj.starttls()
+			smtpObj.login('412313393@qq.com','xwxsxkestgedcajf')
+			if msg.get('Message-ID') in record_list:
+				print('没有新邮件！' + time.strftime('%H:%M',time.localtime()))
+				break
+			else:
+				engine = create_engine('mysql://root:12518ll+.@localhost:3306/test1')
+				db = sessionmaker(bind=engine)
+				session = db()
+				student = session.query(Student).all()
+				mail_list = list(map(lambda x:x.name, student))
+				msg['From'] = '412313393@qq.com'
+				session.close()
+				smtpObj.sendmail('412313393@qq.com',mail_list,msg.as_string())
+				dh = email.header.decode_header(msg.get('subject'))
+				my_subject = dh[0][0].decode(dh[0][1])
+				print('新邮件已发送' + my_subject + time.strftime('%H:%M',time.localtime()))
+				record_list.append(msg.get('Message-ID'))
+				num -= 1
+		revcSer.close()
+		smtpObj.quit()
+		f = open('record.txt', 'wb')
+		pickle.dump(record_list, f)
+		f.close()
+		time.sleep(1000)
+
+if __name__=='__main__':
+	while True:
+		try:
+			main()
+		except BaseException as e:
+			print('获取失败，进入冷却')
+			print(e)
+			time.sleep(1000)
