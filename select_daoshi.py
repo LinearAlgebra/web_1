e6056dc226d21dae6b3ce98653559ab66c39591b
Taitong Xie <412313393@qq.com>
2016-10-01 18:45:13 +0800

    +1s

--- select_daoshi.py
+++ select_daoshi.py
@@ -28,15 +28,19 @@ def index():
 							daoshi_1=form.daoshi_1.data,
 							daoshi_2=form.daoshi_2.data,
 							daoshi_3=form.daoshi_3.data,
-							phone = form.phone.data)
+							phone = form.phone.data,
+							stime = time.strftime("%Y-%m-%d %H:%M:%S"))
 		if student is None:
 			db.session.add(students)
 			db.session.commit()
 			flash('信息录入成功','alert alert-success')
 			return render_template('select_daoshi.html', form=form)
 		else:
+			if print(student) == print(students):
+				flash('该学号已登记，登记时间为%s，你此次输入的信息与数据库中保存的信息相符' % student.stime, 'alert alert-success')
+			else:
+				flash('该学号已登记，登记时间%s, 并且你此次等级的信息与数据库保存的信息不符。如需更改登记信息请微信联系管理员，或用本人南开邮箱发送邮件至2120162310@mail.nankai.edu.cn' % student.stime, 'alert alert-danger')
 			db.session.rollback()
-			flash('该学号已登记，如需更改请微信联系管理员，或用本人南开邮箱发送邮件至2120162310@mail.nankai.edu.cn', 'alert alert-danger')
 			return render_template('select_daoshi.html', form=form)
 	flash('为了防止个人志愿被篡改，在本网页只能提交一次志愿。之后如需要更改，可直接通过微信或者通过南开邮箱联系我2120162310@mail.nankai.edu.cn（发件时请使用本人南开邮箱）','alert alert-info')
 	return render_template('select_daoshi.html', form=form)
@@ -58,6 +62,7 @@ class Student(db.Model):
 	daoshi_2 = db.Column(db.String(6))
 	daoshi_3 = db.Column(db.String(6))
 	phone = db.Column(db.String(11))
+	stime = db.Column(db.String(25))
 
 
 	def __repr__(self):
